c6b8dc5c9333a03c704aba2d878da2ecebf1b5c7
Mohit Joshi <joshicodeshere@gmail.com>
2025-05-16 00:32:51 +0530

    Update GP.c

--- GP.c
+++ GP.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
-    int a, r, n;
+    float a, r, n;
     printf("ENTER THE FIRST TERM : ");
-    scanf("%d",&a);
+    scanf("%f",&a);
     printf("ENTER THE COMMAN RATIO : ");
-    scanf("%d",&r);
+    scanf("%f",&r);
     printf("ENTER THE NUMBER OF TERMS : ");
-    scanf("%d",&n);
+    scanf("%f",&n);
     for(int i=1; i<=n; i+=1){
-        printf("%d ", a);
+        printf("%f ", a);
         a*=r;
     }
     return 0;
